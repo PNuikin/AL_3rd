7bd55d6f8b87c082e7bd7c4356d71cea29c65772
PNuikin <p.nuikin@gmail.com>
2023-09-20 20:52:23 +0300

    added lab 02

---
+++ Lab 02/Task 1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Net.Mail;
+
+namespace Lab_02
+{
+    class Pupil
+    {
+        public virtual void Study()
+        {
+            Console.WriteLine($"Studying: ");
+        }
+
+        public virtual void Read()
+        {
+            Console.WriteLine($"Reading:");
+        }
+
+        public virtual void Write()
+        {
+            Console.WriteLine($"Writing:");
+        }
+
+        public virtual void Relax()
+        {
+            Console.WriteLine($"Relaxing:");
+        }
+    }
+
+    class ExcellentPupil : Pupil
+    {
+        public override void Study()
+        {
+            Console.WriteLine("Studying: Excellent");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("Reading: Excellent");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("Writing: Excellent");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("Relaxing: Excellent");
+        }
+    }
+
+    class GoodPupil : Pupil
+    {
+        public override void Study()
+        {
+            Console.WriteLine("Studying: Good");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("Reading: Good");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("Writing: Good");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("Relaxing: Good");
+        }
+    }
+
+    class BadPupil : Pupil
+    {
+        public override void Study()
+        {
+            Console.WriteLine("Studying: Bad");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine("Reading: Bad");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine("Writing: Bad");
+        }
+
+        public override void Relax()
+        {
+            Console.WriteLine("Relaxing: Bad");
+        }
+    }
+
+    class ClassRoom
+    {
+        private List<Pupil> lst = new List<Pupil>();
+
+        public ClassRoom(params Pupil[] lstin)
+        {
+            foreach (var man in lstin)
+            {
+                lst.Add(man);
+            }
+        }
+
+        public void Print()
+        {
+            int i = 1;
+            foreach (var man in lst)
+            {
+                Console.WriteLine($"{i}) ");
+                man.Study();
+                man.Read();
+                man.Write();
+                man.Relax();
+                i++;
+            }
+        }
+    }
+
+    public static class Program
+    {
+        static void Main()
+        {
+            ExcellentPupil man1 = new();
+            GoodPupil man2 = new(), man3 = new();
+            BadPupil man4 = new();
+            ClassRoom group = new ClassRoom(man1, man2, man3, man4);
+            group.Print();
+        }
+    }
+}
+
---
+++ Lab 02/Task 2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Task_2
+{
+    class Vehicle
+    {
+        private double latitude, longitude;
+        private int price, speed, year;
+
+        public Vehicle(double x, double y, int price, int speed, int year)
+        {
+            latitude = x;
+            longitude = y;
+            this.price = price;
+            this.speed = speed;
+            this.year = year;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Latitude: {latitude}\nLongitude: {longitude}\nPrice: {price}\nSpeed: {speed}\nYear: {year}");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public Car(double x, double y, int price, int speed, int year) : base(x, y, price, speed, year) {}
+
+        public override void Print()
+        {
+            Console.WriteLine("CAR:");
+            base.Print();
+            Console.WriteLine();
+        }
+    }
+
+    class Ship : Vehicle
+    {
+        private int capacity;
+        private string port;
+
+        public Ship(double x, double y, int price, int speed, int year, int capacity, string port) : base(x, y, price,
+            speed, year)
+        {
+            this.capacity = capacity;
+            this.port = port;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("SHIP:");
+            base.Print();
+            Console.WriteLine($"Capacity: {capacity}\nPort: {port}\n");
+        }
+    }
+
+    class Plane : Vehicle
+    {
+        private int height, capacity;
+        
+        public Plane(double x, double y, int price, int speed, int year, int height, int capacity) : base(x, y, price,
+            speed, year)
+        {
+            this.capacity = capacity;
+            this.height = height;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("PLANE:");
+            base.Print();
+            Console.WriteLine($"Height: {height}\nCapacity: {capacity}\n");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car(100, 60, 1000000, 120, 2005);
+            Ship ship = new Ship(30, 20, 25000000, 150, 2020, 300, "Simpheropol");
+            Plane plane = new Plane(40, 70, 30000000, 1100, 2023, 10000, 2);
+            car.Print();
+            ship.Print();
+            plane.Print();
+        }
+    }
+}
+
